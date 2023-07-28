55c6a265c8584e64ac6c9e2289a2442d4c5c651a
Faith Cuneo <faith.cuneo@libertymutual.com>
2023-07-28 09:33:55 -0400

    couple logs and testing git config cleanup

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,8 @@ import './services/analytics.dart';
 class DinderApp extends StatelessWidget {
   DinderApp({super.key});
   final FirebaseAnalyticsObserver observer = MyAnalyticsService.observer;
-
+  //NOTE: If we use named routes, consider adding the route name as a setting in the MaterialPageRoute to make analytics more clear
+  //NOTE: More functionalities are available for areas to observe - skipped remainder of full stack flutter/firebase video on Analytics.
   String get name => 'dinder';
 
   @override
